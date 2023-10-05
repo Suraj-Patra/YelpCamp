9a22108f7e9f3392d708dada0a4aa03a5af32a65
Suraj Patra <patrasuraj2000@gmail.com>
2023-10-05 20:02:45 +0530

    02 MongoDB Setup completed

--- app.js
+++ app.js
@@ -1,24 +1,37 @@
-const path = require('path');
-const express = require('express');
+const path = require("path");
+const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 const PORT = 8000;
 
+/* Local Imports */
+const Campground = require("./models/campground.model");
+
+/* Database */
+mongoose
+  .connect("mongodb://127.0.0.1:27017/yelp-camp")
+  .then((res) => console.log("MongoDB Connected"))
+  .catch((err) => console.log("Error connecting MongoDB", err));
+
 /* View Engine */
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 /* Middlewares */
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
-
-
+app.use(express.urlencoded({ extended: false }));
 
 /* Routers */
-app.get('/', (req, res) => {
-    res.render('home');
-})
-
-
+app.get("/", (req, res) => {
+  res.render("home");
+});
+app.get("/makecampground", async (req, res) => {
+  const camp = await Campground.create({
+    title: "My Backyard",
+    description: "Free camping",
+  });
+  res.send(camp);
+});
 
 app.listen(PORT, () => console.log(`Server running at : ${PORT}`));
